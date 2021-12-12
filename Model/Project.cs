e8d4dc973022496b87aa22b0e8fe909ac95fcd5e
msft <63207978+simplymisfit@users.noreply.github.com>
2021-12-12 11:42:44 +0100

    Initial commit

---
+++ Model/Project.cs
@@ -0,0 +1,14 @@
+namespace Portfolio
+
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
