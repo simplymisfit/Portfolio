1c7715a78b84a39021ae615e7b4860c19a030d35
msft <63207978+simplymisfit@users.noreply.github.com>
2021-12-12 16:53:39 +0100

    Added quotes, saving todo to file, pages, projects

---
+++ Controller/Quotes.cs
@@ -0,0 +1,14 @@
+namespace Portfolio
+
+{
+    public class Quotes
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Quote { get; set; }
+
+        public int Number { get; set; }
+    }
+}
